30964b098b8fafff70ffd4917da8f9009719cd89
Matt Hurne <mhurne@commercehub.com>
2014-05-16 08:46:00 -0400

    Clean up GridFSBlobStoreEndpointSpec

--- src/test/groovy/com/commercehub/jclouds/gridfs/blobstore/GridFSBlobStoreEndpointSpec.groovy
+++ src/test/groovy/com/commercehub/jclouds/gridfs/blobstore/GridFSBlobStoreEndpointSpec.groovy
@@ -6,12 +6,11 @@ import org.jclouds.blobstore.BlobStoreContext
 import spock.lang.Specification
 
 import static com.commercehub.jclouds.gridfs.blobstore.Constants.GRIDFS_URI_SCHEME
+import static org.jclouds.Constants.PROPERTY_ENDPOINT
 
 class GridFSBlobStoreEndpointSpec extends Specification {
     private static final HOST = "localhost"
     private static final PORT = 27017
-    private static final GRIDFS_ENDPOINT = "${GRIDFS_URI_SCHEME}://${HOST}:${PORT}"
-    private static final STANDARD_CONNECTION_STRING = "mongodb://${HOST}:${PORT}"
 
     private BlobStoreContext context
     private BlobStore blobStore
@@ -19,7 +18,7 @@ class GridFSBlobStoreEndpointSpec extends Specification {
     private void initBlobStore(String endpoint) {
         // TODO: use embedded mongo
         def overrides = new Properties()
-        overrides.setProperty(org.jclouds.Constants.PROPERTY_ENDPOINT, endpoint)
+        overrides.setProperty(PROPERTY_ENDPOINT, endpoint)
         context = ContextBuilder.newBuilder("gridfs")
                 .overrides(overrides)
                 .buildView(BlobStoreContext)
@@ -34,11 +33,11 @@ class GridFSBlobStoreEndpointSpec extends Specification {
 
     def "can create blobStore with 'gridfs://' endpoint"() {
         expect:
-        initBlobStore(GRIDFS_ENDPOINT)
+        initBlobStore("${GRIDFS_URI_SCHEME}://${HOST}:${PORT}")
     }
 
     def "can create blobStore with standard connection string endpoint"() {
         expect:
-        initBlobStore(STANDARD_CONNECTION_STRING)
+        initBlobStore("mongodb://${HOST}:${PORT}")
     }
 }
